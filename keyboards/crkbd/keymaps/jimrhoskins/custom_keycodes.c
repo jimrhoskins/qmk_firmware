04a27ce499ed9c3dea99f9f72bcce5f2b5d509d5
Jim <jim@jimhoskins.com>
2024-05-10 03:11:19 +0000

    remove comments

--- keyboards/crkbd/keymaps/jimrhoskins/custom_keycodes.c
+++ keyboards/crkbd/keymaps/jimrhoskins/custom_keycodes.c
@@ -4,26 +4,26 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case LALT_T(KC_CIRC):
             if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_CIRC); // Send KC_DQUO on tap
-                return false;        // Return false to ignore further processing of key
+                tap_code16(KC_CIRC);
+                return false;
             }
             break;
         case LCTL_T(KC_AMPR):
             if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_AMPR); // Send KC_DQUO on tap
-                return false;        // Return false to ignore further processing of key
+                tap_code16(KC_AMPR);
+                return false;
             }
             break;
         case LGUI_T(KC_ASTR):
             if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_ASTR); // Send KC_DQUO on tap
-                return false;        // Return false to ignore further processing of key
+                tap_code16(KC_ASTR);
+                return false;
             }
             break;
         case LSFT_T(KC_LPRN):
             if (record->tap.count && record->event.pressed) {
-                tap_code16(KC_LPRN); // Send KC_DQUO on tap
-                return false;        // Return false to ignore further processing of key
+                tap_code16(KC_LPRN);
+                return false;
             }
             break;
     }
