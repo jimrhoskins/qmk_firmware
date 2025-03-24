d807b82e9fd126d4289b656b0cafcdc517b7cb78
Jim Hoskins <jim@jimhoskins.com>
2025-03-24 14:15:26 -0700

    Configure chordal hold

--- keyboards/crkbd/keymaps/jimrhoskins/keymap.c
+++ keyboards/crkbd/keymaps/jimrhoskins/keymap.c
@@ -43,6 +43,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 };
 
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM = LAYOUT_split_3x5_3(
+    'L','L','L','L','L',  'R','R','R','R','R',
+    'L','L','*','L','L',  'R','R','R','R','R',
+    'L','L','L','L','L',  'R','R','R','R','R',
+            '*','*','*',  '*','*','*'
+);
+
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [ALPHAS] = {
